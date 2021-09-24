cd32612d2218aca902742f8938b64cf36118dc40
fatimaazzam <fatimaazzam1@gmail.com>
2021-09-24 13:02:25 +0300

    Update ModeratorController.java

--- natter-api/src/main/java/com/manning/apisecurityinaction/controller/ModeratorController.java
+++ natter-api/src/main/java/com/manning/apisecurityinaction/controller/ModeratorController.java
@@ -13,15 +13,15 @@ public class ModeratorController {
     this.database = database;
   }
 
-  //Description of function and parameters is not added
+  
   public JSONObject deletePost(Request request, Response response) {
     
-    //spaceId should not be added in url and it can be added if it is encrypted
+   
     var spaceId = Long.parseLong(request.params(":spaceId"));
     
-    //msgId should not be added in url and it can be added if it is encrypted
+   
     var msgId = Long.parseLong(request.params(":msgId"));
-    //SQL Injection
+    
     database.updateUnique("DELETE FROM messages " +
         "WHERE space_id = ? AND msg_id = ?", spaceId, msgId);
     response.status(200);
